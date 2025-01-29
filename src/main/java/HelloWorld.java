ad3ae435b4de9428e1d6cd3cf65232191ebc720b
inesaaaa <Inesa.Afitsaryan@am-unit-0108.yes.labs.intellij.net>
2025-01-29 12:07:28 +0400

    Inital commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Qodana!");
+    }
+
+    // Example method with poor code practices to test Qodana warnings
+    public void sampleMethod() {
+        int x = 5; // Unused variable
+        System.out.println("This is a sample method");
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class HelloWorldTest {
+    @Test
+    public void testHelloWorld() {
+        assertTrue(true); // Example test, always passing
+    }
+}
